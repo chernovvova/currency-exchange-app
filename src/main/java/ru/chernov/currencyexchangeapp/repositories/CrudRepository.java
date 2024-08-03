cacb4f34baf3253ced237e7b8c304c966aec75b3
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-03 21:07:13 +0300

    feat: add CrudRepository interface

---
+++ src/main/java/ru/chernov/currencyexchangeapp/repositories/CrudRepository.java
@@ -0,0 +1,16 @@
+package ru.chernov.currencyexchangeapp.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CrudRepository<T> {
+    Optional<T> findById(Long id);
+
+    List<T> findAll();
+
+    T save(T entity);
+
+    void delete(Long id);
+
+    void update(T entity);
+}
