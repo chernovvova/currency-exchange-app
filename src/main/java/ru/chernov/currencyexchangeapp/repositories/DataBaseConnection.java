1e1f35b2403fb96e68d60665479925311372dd44
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-04 17:59:12 +0300

    feat: add DataBaseConnection class
    singleton class that contains connection with database

---
+++ src/main/java/ru/chernov/currencyexchangeapp/repositories/DataBaseConnection.java
@@ -0,0 +1,30 @@
+package ru.chernov.currencyexchangeapp.repositories;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection {
+    private static final String DRIVER_NAME = "org.sqlite.JDBC";
+    private Connection connection = null;
+
+    public DataBaseConnection() {
+        if(connection == null){
+            try {
+                Class.forName(DRIVER_NAME);
+                connection = DriverManager.getConnection("jdbc:sqlite:/Users/vova/IdeaProjects/currency-exchange-app/src/main/resources/db/currency_exchange_app.sqlite");
+
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void closeConnection() throws SQLException {
+        connection.close();
+    }
+}
