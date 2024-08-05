e89326fa982a203d3a41d24d8c1f360bc5cbd75a
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-05 13:52:28 +0300

    feat: add ErrorHandler class
    add class that generate error responses for REST API methods

---
+++ src/main/java/ru/chernov/currencyexchangeapp/utils/ErrorHandler.java
@@ -0,0 +1,17 @@
+package ru.chernov.currencyexchangeapp.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ErrorHandler {
+    public static void handleError(int code, String message, HttpServletResponse response) {
+        try {
+            response.setStatus(code);
+            response.getWriter().println((new ObjectMapper()).createObjectNode().put("message", message));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
