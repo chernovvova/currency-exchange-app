87e4c6bdf4bc611f4599b970f36be0745d010159
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-05 13:52:50 +0300

    feat: add new error responses for doGet method

--- src/main/java/ru/chernov/currencyexchangeapp/servlets/CurrenciesServlet.java
+++ src/main/java/ru/chernov/currencyexchangeapp/servlets/CurrenciesServlet.java
@@ -3,6 +3,7 @@ package ru.chernov.currencyexchangeapp.servlets;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.chernov.currencyexchangeapp.models.Currency;
 import ru.chernov.currencyexchangeapp.repositories.CurrencyRepositoryImpl;
+import ru.chernov.currencyexchangeapp.utils.ErrorHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,13 +11,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "CurrenciesServlet", urlPatterns = "/currencies")
 public class CurrenciesServlet extends HttpServlet {
     CurrencyRepositoryImpl currencyRepository = new CurrencyRepositoryImpl();
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        new ObjectMapper().writeValue(resp.getWriter(), currencyRepository.findAll());
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp){
+        try {
+            new ObjectMapper().writeValue(resp.getWriter(), currencyRepository.findAll());
+        } catch(SQLException e) {
+            ErrorHandler.handleError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Database Error", resp);
+        } catch (Exception e) {
+            ErrorHandler.handleError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Fatal error", resp);
+        }
     }
 
     @Override
