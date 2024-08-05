367f40c475a5fb43a8bedb9666141af45c709e97
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-05 17:33:34 +0300

    feat: add new error responses for doGet method

--- src/main/java/ru/chernov/currencyexchangeapp/servlets/CurrencyServlet.java
+++ src/main/java/ru/chernov/currencyexchangeapp/servlets/CurrencyServlet.java
@@ -3,6 +3,8 @@ package ru.chernov.currencyexchangeapp.servlets;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.chernov.currencyexchangeapp.models.Currency;
 import ru.chernov.currencyexchangeapp.repositories.CurrencyRepositoryImpl;
+import ru.chernov.currencyexchangeapp.utils.ErrorHandler;
+import ru.chernov.currencyexchangeapp.utils.Validator;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Optional;
 
 @WebServlet(name = "CurrencyServlet", urlPatterns = "/currency/*")
@@ -18,10 +21,25 @@ public class CurrencyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String code = req.getPathInfo().replaceFirst("/", "").toUpperCase();
-        Optional<Currency> optionalCurrencyDTO = currencyRepository.findByCode(code);
 
-        if (optionalCurrencyDTO.isPresent()) {
-            new ObjectMapper().writeValue(resp.getWriter(), optionalCurrencyDTO.get());
+        try {
+            if(Validator.validateCurrencyCode(code)) {
+                Optional<Currency> optionalCurrencyDTO = currencyRepository.findByCode(code);
+                if (optionalCurrencyDTO.isPresent()) {
+                    resp.setStatus(HttpServletResponse.SC_OK);
+                    new ObjectMapper().writeValue(resp.getWriter(), optionalCurrencyDTO.get());
+                }
+                else {
+                    ErrorHandler.handleError(HttpServletResponse.SC_NOT_FOUND, "Currency not found", resp);
+                }
+            }
+            else {
+                ErrorHandler.handleError(HttpServletResponse.SC_BAD_REQUEST, "Invalid currency code", resp);
+            }
+        } catch (SQLException e) {
+            ErrorHandler.handleError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Database error", resp);
+        } catch (Exception e) {
+            ErrorHandler.handleError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Fatal error", resp);
         }
     }
 }
