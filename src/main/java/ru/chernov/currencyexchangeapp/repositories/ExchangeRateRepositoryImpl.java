497f0499359cb3ca0926590c6e27ea6103354491
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-05 20:02:51 +0300

    feat: implement save method

--- src/main/java/ru/chernov/currencyexchangeapp/repositories/ExchangeRateRepositoryImpl.java
+++ src/main/java/ru/chernov/currencyexchangeapp/repositories/ExchangeRateRepositoryImpl.java
@@ -5,10 +5,7 @@ import ru.chernov.currencyexchangeapp.models.ExchangeRate;
 
 import javax.xml.transform.Result;
 import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -23,10 +20,10 @@ public class ExchangeRateRepositoryImpl implements ExchangeRateRepository{
     @Override
     public List<ExchangeRate> findAll() throws SQLException {
         final String query = "SELECT exchange_rates.id AS id," +
-                " base.id AS base_id, base.code AS base_code, base.full_name AS base_name, base.sign AS base_sign, " +
-                " target.id AS target_id, target.code AS target_code, target.full_name AS target_name, target.sign AS target_sign, rate " +
-                "FROM exchange_rates JOIN currencies base ON base_currency_id = base.id " +
-                "JOIN currencies target ON target_currency_id = target.id";
+                                " base.id AS base_id, base.code AS base_code, base.full_name AS base_name, base.sign AS base_sign, " +
+                                " target.id AS target_id, target.code AS target_code, target.full_name AS target_name, target.sign AS target_sign, rate " +
+                            "FROM exchange_rates JOIN currencies base ON base_currency_id = base.id " +
+                            "JOIN currencies target ON target_currency_id = target.id";
         DataBaseConnection dataBaseConnection = new DataBaseConnection();
         Connection connection = dataBaseConnection.getConnection();
 
@@ -44,6 +41,27 @@ public class ExchangeRateRepositoryImpl implements ExchangeRateRepository{
 
     @Override
     public ExchangeRate save(ExchangeRate entity) throws SQLException {
+        final String query = "INSERT INTO exchange_rates VALUES (?, ?, ?, ?)";
+        DataBaseConnection dataBaseConnection = new DataBaseConnection();
+        Connection connection = dataBaseConnection.getConnection();
+
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(2, entity.getBaseCurrency().getCode());
+        preparedStatement.setString(3, entity.getTargetCurrency().getCode());
+        preparedStatement.setBigDecimal(4, entity.getRate());
+
+        int saveResult = preparedStatement.executeUpdate();
+
+        Long id = null;
+        Optional<ExchangeRate> exchangeRate = Optional.empty();
+
+        if (saveResult > 0) {
+            exchangeRate = findByCodePair(entity.getBaseCurrency().getCode(), entity.getTargetCurrency().getCode());
+            if (exchangeRate.isPresent()) {
+                return exchangeRate.get();
+            }
+        }
+
         return null;
     }
 
@@ -55,6 +73,28 @@ public class ExchangeRateRepositoryImpl implements ExchangeRateRepository{
     @Override
     public void update(ExchangeRate entity) throws SQLException {
 
+    }
+
+    @Override
+    public Optional<ExchangeRate> findByCodePair(String baseCode, String targetCode) throws SQLException {
+        final String query = "SELECT exchange_rates.id AS id," +
+                " base.id AS base_id, base.code AS base_code, base.full_name AS base_name, base.sign AS base_sign, " +
+                " target.id AS target_id, target.code AS target_code, target.full_name AS target_name, target.sign AS target_sign, rate " +
+                "FROM exchange_rates JOIN currencies base ON base_currency_id = base.id " +
+                "JOIN currencies target ON target_currency_id = target.id" +
+                "WHERE base_code = ? AND target_code = ?";
+        DataBaseConnection dataBaseConnection = new DataBaseConnection();
+        Connection connection = dataBaseConnection.getConnection();
+
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, baseCode);
+        preparedStatement.setString(2, targetCode);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        Optional<ExchangeRate> exchangeRate = Optional.of(getExchangeRate(resultSet));
+
+        return exchangeRate;
     }
 
     private ExchangeRate getExchangeRate(ResultSet resultSet) throws SQLException {
