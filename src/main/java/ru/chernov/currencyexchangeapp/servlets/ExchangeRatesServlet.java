0e88f97a195adb8a149e6edc4a8178ace64595a1
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-05 21:01:08 +0300

    feat: implement doPost method
    method saves new exchange rate

--- src/main/java/ru/chernov/currencyexchangeapp/servlets/ExchangeRatesServlet.java
+++ src/main/java/ru/chernov/currencyexchangeapp/servlets/ExchangeRatesServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -29,6 +30,38 @@ public class ExchangeRatesServlet extends HttpServlet {
             e.printStackTrace();
         } catch (Exception e) {
             ErrorHandler.handleError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Fatal error", resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String baseCurrencyCode = req.getParameter("baseCurrencyCode");
+        String targetCurrencyCode = req.getParameter("targetCurrencyCode");
+        BigDecimal rate = new BigDecimal(Double.parseDouble(req.getParameter("rate")));
+
+        if (baseCurrencyCode == null || targetCurrencyCode == null || rate == null) {
+            ErrorHandler.handleError(HttpServletResponse.SC_BAD_REQUEST, "Missing parameters", resp);
+        }
+        else {
+            try {
+                if(exchangeRateRepository.findByCodePair(baseCurrencyCode, targetCurrencyCode).isPresent()) {
+                    ErrorHandler.handleError(HttpServletResponse.SC_CONFLICT, "Exchange rate already exists", resp);
+                }
+                else {
+                    ExchangeRate exchangeRate = new ExchangeRate();
+                    exchangeRate.getBaseCurrency().setCode(baseCurrencyCode);
+                    exchangeRate.getTargetCurrency().setCode(targetCurrencyCode);
+                    exchangeRate.setRate(rate);
+                    exchangeRate = exchangeRateRepository.save(exchangeRate);
+
+                    new ObjectMapper().writeValue(resp.getWriter(), exchangeRate);
+                    resp.setStatus(HttpServletResponse.SC_CREATED);
+                }
+            } catch (SQLException e) {
+                ErrorHandler.handleError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Database error", resp);
+            } catch (Exception e) {
+                ErrorHandler.handleError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Fatal error", resp);
+            }
         }
     }
 }
