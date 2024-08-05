b6969fb2c39febd381301d8c5d453d63b51943d8
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-05 17:45:48 +0300

    feat: add ExchangeRate class

---
+++ src/main/java/ru/chernov/currencyexchangeapp/models/ExchangeRate.java
@@ -0,0 +1,57 @@
+package ru.chernov.currencyexchangeapp.models;
+
+public class ExchangeRate {
+    Long id;
+    Currency baseCurrency;
+    Currency targetCurrency;
+    double rate;
+
+    public ExchangeRate(Long id, Currency baseCurrency, Currency targetCurrency, double rate) {
+        this.id = id;
+        this.baseCurrency = baseCurrency;
+        this.targetCurrency = targetCurrency;
+        this.rate = rate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Currency getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public void setBaseCurrency(Currency baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
+
+    public Currency getTargetCurrency() {
+        return targetCurrency;
+    }
+
+    public void setTargetCurrency(Currency targetCurrency) {
+        this.targetCurrency = targetCurrency;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    @Override
+    public String toString() {
+        return "ExchangeRate{" +
+                "id=" + id +
+                ", baseCurrency=" + baseCurrency +
+                ", targetCurrency=" + targetCurrency +
+                ", rate=" + rate +
+                '}';
+    }
+}
