2ea273f496362f95f2f9991b0054f2d48a50f5b8
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-05 20:02:16 +0300

    refactor: add new method
    findByCode pairs method returns exchange rate from db by code pair

--- src/main/java/ru/chernov/currencyexchangeapp/repositories/ExchangeRateRepository.java
+++ src/main/java/ru/chernov/currencyexchangeapp/repositories/ExchangeRateRepository.java
@@ -2,5 +2,10 @@ package ru.chernov.currencyexchangeapp.repositories;
 
 import ru.chernov.currencyexchangeapp.models.ExchangeRate;
 
+import java.sql.SQLException;
+import java.util.Optional;
+import java.util.OptionalInt;
+
 public interface ExchangeRateRepository extends CrudRepository<ExchangeRate> {
+    Optional<ExchangeRate> findByCodePair(String baseCode, String targetCode) throws SQLException;
 }
