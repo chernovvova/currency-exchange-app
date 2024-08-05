91abc36d9c02361ee03ddc813a32a3f0af19d59c
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-05 18:55:13 +0300

    feat: implement doGet method
    method returns all exchange rates from database or error code and message

--- src/main/java/ru/chernov/currencyexchangeapp/servlets/ExchangeRatesServlet.java
+++ src/main/java/ru/chernov/currencyexchangeapp/servlets/ExchangeRatesServlet.java
@@ -1,14 +1,34 @@
 package ru.chernov.currencyexchangeapp.servlets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ru.chernov.currencyexchangeapp.models.ExchangeRate;
+import ru.chernov.currencyexchangeapp.repositories.ExchangeRateRepository;
+import ru.chernov.currencyexchangeapp.repositories.ExchangeRateRepositoryImpl;
+import ru.chernov.currencyexchangeapp.utils.ErrorHandler;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
+@WebServlet(name = "ExchangeRatesServlet", urlPatterns = "/exchangeRates")
 public class ExchangeRatesServlet extends HttpServlet {
+    ExchangeRateRepository exchangeRateRepository = new ExchangeRateRepositoryImpl();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        try {
+            List<ExchangeRate> exchangeRates = exchangeRateRepository.findAll();
+            new ObjectMapper().writeValue(resp.getWriter(), exchangeRates);
+            resp.setStatus(HttpServletResponse.SC_OK);
+        } catch (SQLException e) {
+            ErrorHandler.handleError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Database error", resp);
+            e.printStackTrace();
+        } catch (Exception e) {
+            ErrorHandler.handleError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Fatal error", resp);
+        }
     }
 }
