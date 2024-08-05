97e8fba71465dd7ebeb4b4a2e3fdd4ea8f65c6a3
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-05 18:36:32 +0300

    feat: add ExchangeRateRepository interface

---
+++ src/main/java/ru/chernov/currencyexchangeapp/repositories/ExchangeRateRepository.java
@@ -0,0 +1,6 @@
+package ru.chernov.currencyexchangeapp.repositories;
+
+import ru.chernov.currencyexchangeapp.models.ExchangeRate;
+
+public interface ExchangeRateRepository extends CrudRepository<ExchangeRate> {
+}
