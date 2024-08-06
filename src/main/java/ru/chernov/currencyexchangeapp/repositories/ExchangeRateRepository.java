7ee14edaffb70db1ea33fb16026fd2e688da2791
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-06 10:47:14 +0300

    fix: remove unused import

--- src/main/java/ru/chernov/currencyexchangeapp/repositories/ExchangeRateRepository.java
+++ src/main/java/ru/chernov/currencyexchangeapp/repositories/ExchangeRateRepository.java
@@ -4,7 +4,6 @@ import ru.chernov.currencyexchangeapp.models.ExchangeRate;
 
 import java.sql.SQLException;
 import java.util.Optional;
-import java.util.OptionalInt;
 
 public interface ExchangeRateRepository extends CrudRepository<ExchangeRate> {
     Optional<ExchangeRate> findByCodePair(String baseCode, String targetCode) throws SQLException;
