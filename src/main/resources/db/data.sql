cf7e73004034e91f68c83899cd1bc463cfd02718
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-06 18:17:25 +0300

    refactor: remove insert queries

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -2,10 +2,5 @@ INSERT INTO currencies(code, full_name, sign) VALUES ("EUR", "Euro", "€");
 INSERT INTO currencies(code, full_name, sign) VALUES ("RUB", "Russian Ruble", "₽");
 INSERT INTO currencies(code, full_name, sign) VALUES ("USD", "US Dollar", "$");
 
-INSERT INTO exchange_rates(base_currency_id, target_currency_id, rate) VALUES (3, 2, 80);
-INSERT INTO exchange_rates(base_currency_id, target_currency_id, rate) VALUES (1, 2, 90);
-INSERT INTO exchange_rates(base_currency_id, target_currency_id, rate) VALUES (1, 3, 1.1);
-
-
 SELECT * FROM currencies;
 SELECT * FROM exchange_rates;
