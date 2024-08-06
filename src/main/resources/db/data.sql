1c1812ecd8f711cdbfa08ef759bfa16267940d18
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-06 22:51:35 +0300

    feat: add new insert queries for currencies

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,6 +1,11 @@
 INSERT INTO currencies(code, full_name, sign) VALUES ("EUR", "Euro", "€");
 INSERT INTO currencies(code, full_name, sign) VALUES ("RUB", "Russian Ruble", "₽");
 INSERT INTO currencies(code, full_name, sign) VALUES ("USD", "US Dollar", "$");
+INSERT INTO currencies(code, full_name, sign) VALUES ("AFN", "Afghani", "؋");
+INSERT INTO currencies(code, full_name, sign) VALUES ("XCD", "East Caribbean Dollar", "EC$");
+INSERT INTO currencies(code, full_name, sign) VALUES ("AOA", "Kwanza", "Kz");
+INSERT INTO currencies(code, full_name, sign) VALUES ("AMD", "Armenian Dram", "֏");
+INSERT INTO currencies(code, full_name, sign) VALUES ("AWG", "Aruban Florin", "Aƒ");
 
 SELECT * FROM currencies;
 SELECT * FROM exchange_rates;
